203d84ab2b06e529d1eac94f2071cd96893a5797
Nico Bonanno <bonannon@wit.edu>
2026-02-27 12:11:57 -0500

    Adding code that cleans the data, calculates price per square foot, and takes the log transformation of sale price to reduce skew.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Data-Science-NYC-Housing-Project
+Nico Bonanno, Jake Samela, Emraan Kafihi
+
 This is the final project for our Data Science Fundamentals course at Wentworth Institute of Technology. Our project is analyzing New York City housing data to answer five questions which will be specified later.
+
+Intro:
+- Why was the project undertaken?
+
+- Research Questions:
+1) Do sale prices differ significantly by borough?
+2) Does the number of residential units affect sale price per square foot?
+3) Is zip code a determinant of sale price?
+4) Does building age affect sale price?
+5) Can we accurately predict the sale price using building characteristics?
+
+- Purpose:
+The purpose of our research was to determine the answers to the above questions and to gather information on the housing situation in New York City, one of the largest cities in the world. 
--- analysis.py
+++ analysis.py
@@ -1,6 +1,32 @@
-#Read data
+#Imports
+import numpy as np
 import pandas as pd
 
+#Read data
 df = pd.read_csv("Dataset/nyc_housing_base.csv")
 
-print(df.head())
+#Drop rows with missing values. 236 rows were dropped which is only about 0.7% of the dataset
+df = df.dropna()
+
+#Inspect data
+print("Shape:", df.shape)
+print("\nColumns:")
+print(df.columns)
+
+print("\nMissing values:")
+print(df.isnull().sum())
+
+print((df["sale_price"] == 0).sum())
+print((df["bldgarea"] == 0).sum())
+
+print("Sale price skew:", df["sale_price"].skew())
+
+#Take log transform of sale price to reduce skewness and help with regression
+df["log_sale_price"] = np.log(df["sale_price"])
+
+#Compare skew of sale price before and after log transform
+print("Original sale price skew:", df["sale_price"].skew())
+print("Log sale price skew:", df["log_sale_price"].skew())
+
+#Feature engineering - create price per square foot feature
+df["price_per_sqft"] = df["sale_price"] / df["bldgarea"]
